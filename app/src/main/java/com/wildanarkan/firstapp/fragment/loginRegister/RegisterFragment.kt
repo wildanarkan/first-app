ebfd52ca3bde0fd7d5737e1e84c17bd0e0a6e370
wildanarkan <cillmystic@gmail.com>
2024-01-03 16:40:06 +0700

    feat : fragment_introduction.xml

---
+++ app/src/main/java/com/wildanarkan/firstapp/fragment/loginRegister/RegisterFragment.kt
@@ -0,0 +1,13 @@
+package com.wildanarkan.firstapp.fragment.loginRegister
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wildanarkan.firstapp.R
+
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+}
